da3259f77ccfca8c80e695a3881961fb7831d007
Wen-ChuangChou <132545007+Wen-ChuangChou@users.noreply.github.com>
2023-06-02 21:25:50 +0200

    file created

---
+++ model.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Classifier(nn.Module):
+    def __init__(self, d_model=80, n_spks=600, dropout=0.1):
+        super().__init__()
+        # Project the dimension of features from that of input into d_model.
+        self.prenet = nn.Linear(40, d_model)
+
+        self.encoder_layer = nn.TransformerEncoderLayer(
+            d_model=d_model, dim_feedforward=256, nhead=2
+        )
+
+        # Project the the dimension of features from d_model into speaker nums.
+        self.pred_layer = nn.Sequential(
+            nn.Linear(d_model, d_model),
+            nn.Sigmoid(),
+            nn.Linear(d_model, n_spks),
+        )
+
+    def forward(self, mels):
+        """
+        args:
+                mels: (batch size, length, 40)
+        return:
+                out: (batch size, n_spks)
+        """
+        # out: (batch size, length, d_model)
+        out = self.prenet(mels)
+        # out: (length, batch size, d_model)
+        out = out.permute(1, 0, 2)
+        # The encoder layer expect features in the shape of (length, batch size, d_model).
+        out = self.encoder_layer(out)
+        # out: (batch size, length, d_model)
+        out = out.transpose(0, 1)
+        # mean pooling
+        stats = out.mean(dim=1)
+
+        # out: (batch, n_spks)
+        out = self.pred_layer(stats)
+        return out
