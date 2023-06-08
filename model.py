d6fd503e279a8b5d84f3536017d25ee50c6c4f08
Wen-ChuangChou <132545007+Wen-ChuangChou@users.noreply.github.com>
2023-06-08 07:50:57 +0200

    Update model.py

--- model.py
+++ model.py
@@ -4,19 +4,20 @@ import torch.nn.functional as F
 
 
 class Classifier(nn.Module):
-    def __init__(self, d_model=80, n_spks=600, dropout=0.1):
+    def __init__(self, d_model=256, n_spks=600, dropout=0.1):
         super().__init__()
         # Project the dimension of features from that of input into d_model.
         self.prenet = nn.Linear(40, d_model)
 
         self.encoder_layer = nn.TransformerEncoderLayer(
-            d_model=d_model, dim_feedforward=256, nhead=2
+            d_model=d_model, nhead=128, dim_feedforward=2048, dropout=dropout
         )
+        self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=2)
 
         # Project the the dimension of features from d_model into speaker nums.
         self.pred_layer = nn.Sequential(
             nn.Linear(d_model, d_model),
-            nn.Sigmoid(),
+            nn.ReLU(),
             nn.Linear(d_model, n_spks),
         )
 
